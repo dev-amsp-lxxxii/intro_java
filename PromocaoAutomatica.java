f22d964f68c6000813f04f3dc0cc2d8a43c97c47
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2025-08-22 20:05:40 -0300

    Conclusão do exercício de Promoção Automática

---
+++ PromocaoAutomatica.java
@@ -0,0 +1,16 @@
+public class PromocaoAutomatica {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 20;
+        char c = 'A';
+        int d = 30;
+        long e = 40L;
+        float f = 1.5f;
+        double g = 2.5;
+
+        double resultado = a + b;
+        System.out.println("Resultado: " + resultado);
+        System.out.println("byte a: " + a);
+        System.out.println("Char c: " + c);
+    }
+}
