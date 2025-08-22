09923a6ffc4a749a9b177c9fe5e8b370d0dd2ef4
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2025-08-22 19:15:37 -0300

    Conclusão do exercício de Área do Retangulo

---
+++ CalculadoraAreaRetangulo.java
@@ -0,0 +1,10 @@
+public class CalculadoraAreaRetangulo {
+    public static void main(String[] args) {
+        double altura = 5.0;
+        double largura = 3.0;
+
+        double areaRetangulo = largura * altura;
+
+        System.out.println("A área do RETANGULO é: " + areaRetangulo);
+    }
+}
