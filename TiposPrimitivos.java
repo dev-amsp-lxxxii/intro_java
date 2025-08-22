4f8762d868f1a5ec8ca398ee9ef9b74e0eb97300
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2025-08-22 14:53:22 -0300

    Seção 10: Fundamentos de Programação Java - 37 Tipos Primitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,21 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        byte idade = 25;
+        short ano = 2025;
+        int populacaoCidade = 500000;
+        long populacaoMundial = 7800000000L;
+        float altura = 1.75f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char inicialNome = 'A';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População Cidade: " + populacaoCidade);
+        System.out.println("População Mundial: " + populacaoMundial);
+        System.out.println("Altura" + altura);
+        System.out.println("Salário: " + salario);
+        System.out.println("Estudante: " + estudante);
+        System.out.println("Inicial Nome: " + inicialNome);
+    }
+}
