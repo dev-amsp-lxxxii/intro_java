abfe41156d303d1f6c29ec1577e87d1f7737a403
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2025-08-22 19:03:26 -0300

    Conclusão do exercicio de Controle de Estoque de Padaria

---
+++ ControleEstoquePadaria.java
@@ -0,0 +1,9 @@
+public class ControleEstoquePadaria {
+    public static void main(String[] args) {
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+
+        System.out.println("O estoque atual de pacotes de FARINHA é: " + quantidadeFarinha + " unidades.");
+        System.out.println("O estoque atual de pacotes de AÇUCAR é: " + quantidadeAcucar + " unidades.");
+    }
+}
