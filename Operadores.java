5827e05d204f0e8e9d7f0969960b6c76a2cc8fc2
Adelmo Menezes - Opensuse <ams.protazio@gmail.com>
2025-08-23 16:37:25 -0300

    Seção 11: Operadores - 51 Operadores Aritméticos

---
+++ Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
