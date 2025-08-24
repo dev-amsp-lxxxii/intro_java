91699682d0d3bd807dc1d0afb79d85345d133450
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-24 15:45:35 -0300

    52. Operadores de Comparação

--- Operadores.java
+++ Operadores.java
@@ -1,4 +1,6 @@
+
 public class Operadores {
+
     public static void main(String[] args) {
         // Operadores Aritméticos
         int aOpAri = 10;
@@ -9,5 +11,17 @@ public class Operadores {
         System.out.println(aOpAri * bOpAri);
         System.out.println(aOpAri / bOpAri);
         System.out.println(aOpAri % bOpAri);
+
+        // Operadores de Compraração
+        int aOpComp = 10;
+        int bOpComp = 20;
+
+        System.out.println(aOpComp == bOpComp);
+        System.out.println(aOpComp != bOpComp);
+        System.out.println(aOpComp > bOpComp);
+        System.out.println(aOpComp < bOpComp);
+        System.out.println(aOpComp >= bOpComp);
+        System.out.println(aOpComp <= bOpComp);
     }
+
 }
