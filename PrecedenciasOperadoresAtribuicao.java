21d85a7e64052d54d7ffa146a1f9d162286340e0
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-24 21:23:49 -0300

    Conclusão do exercício de Precedencia

---
+++ PrecedenciasOperadoresAtribuicao.java
@@ -0,0 +1,19 @@
+
+public class PrecedenciasOperadoresAtribuicao {
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        ++numero;
+        System.out.println(numero);
+
+        numero++;
+        System.out.println(numero);
+
+        --numero;
+        System.out.println(numero);
+
+        numero--;
+        System.out.println(numero);
+    }
+}
