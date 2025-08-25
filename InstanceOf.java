f2d5824f098c0eb4ea3c75715d4afe8c1f1f0a1e
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-25 15:40:21 -0300

    65. instanceof

---
+++ InstanceOf.java
@@ -0,0 +1,12 @@
+
+public class InstanceOf {
+
+    // Instrunções Condicionais em Objetos
+    public static void main(String[] args) {
+        Object obj = "Olá, Adelmo Menezes!";
+
+        if (obj instanceof String) {
+            System.out.println(obj);
+        }
+    }
+}
