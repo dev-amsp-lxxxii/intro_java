4a0eedf381a9eed1fe0e3bc9b26f2fd4e9a4a1f6
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-25 15:45:29 -0300

    66. Exercicio de par ou impar resolvido

---
+++ ExNumPar.java
@@ -0,0 +1,14 @@
+
+public class ExNumPar {
+
+    public static void main(String[] args) {
+        int valor = 10;
+
+        if (valor % 2 == 0) {
+            System.out.println("O número: " + valor + " é PAR");
+        } else {
+            System.out.println("O número: " + valor + " é IMPAR");
+        }
+    }
+
+}
