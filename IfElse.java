f3e4c2de43868108d81301321457cd2a3d6619d4
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-25 15:24:51 -0300

    63 - if-else

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+
+public class IfElse {
+
+    public static void main(String[] args) {
+        int a = 10;
+
+        if (a > 5) {
+            System.out.println("A é maior que 5");
+        } else {
+            System.out.println("A é menor ou igual a 5");
+        }
+    }
+}
