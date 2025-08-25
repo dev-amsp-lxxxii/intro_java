de1b9d604b45357495dec4cae529da132e0b87ae
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-24 21:03:06 -0300

    Exercicio de Operadores de Atribuição concluida

---
+++ OperadoresAtribuicaoExercicio.java
@@ -0,0 +1,14 @@
+
+public class OperadoresAtribuicaoExercicio {
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero += 3;
+        numero -= 2;
+        numero *= 4;
+        numero *= 2;
+
+        System.out.println(numero);
+    }
+}
