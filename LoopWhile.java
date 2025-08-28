42256884bc7837abfd7eb98a55b0f764f2d219d0
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-28 17:46:58 -0300

    80. Loop While

---
+++ LoopWhile.java
@@ -0,0 +1,12 @@
+
+public class LoopWhile {
+
+    public static void main(String[] args) {
+        int j = 0;
+
+        while (j < 5) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
