a92e949e2df544cedddc8ba79e82f8669468897e
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-28 17:54:17 -0300

    81. Loop do-while

---
+++ LoopDoWhile.java
@@ -0,0 +1,12 @@
+
+public class LoopDoWhile {
+
+    public static void main(String[] args) {
+        int k = 30;
+
+        do {
+            System.out.println(k);
+            k++;
+        } while (k < 5);
+    }
+}
