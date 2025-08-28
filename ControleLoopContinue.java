c32bbad04ee215d214a24d4f917e877f54d73f0c
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-28 20:23:54 -0300

    83. Controle de loop - continue

---
+++ ControleLoopContinue.java
@@ -0,0 +1,12 @@
+
+public class ControleLoopContinue {
+
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            if (j == 5) {
+                continue;
+            }
+            System.out.println(j);
+        }
+    }
+}
