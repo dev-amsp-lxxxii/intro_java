e063290023cd1c93e6b77122fd23d579b2541816
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-29 18:53:21 -0300

    91. Classes e Objetos

---
+++ Bolo.java
@@ -0,0 +1,19 @@
+public class Bolo {
+    // variável de instância ou variável menbro
+    // inicicialização padrão -> zero
+    int quantidadeAcucar = 288;
+
+    public static void main(String[] args) {
+        Bolo boloDeChocolate = new Bolo();
+        boloDeChocolate.quantidadeAcucar = 200;
+
+        Bolo boloDeBaunilha = new Bolo();
+        boloDeBaunilha.quantidadeAcucar = 150;
+
+        Bolo boloDeMacaxeira = new Bolo();
+
+        System.out.println("Quantidade de açucar no bolo de chocolate: " + boloDeChocolate.quantidadeAcucar);
+        System.out.println("Quantidade de açucar no bolo de baunilha: " + boloDeBaunilha.quantidadeAcucar);
+        System.out.println("Quantidade de açucar no bolo de macaxeira: " + boloDeMacaxeira.quantidadeAcucar);
+    }
+}
