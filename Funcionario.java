465da4ca12ef46b8d145d7e7c0a978ef9f9d56a2
Adelmo Dhelldev <adelmo.msp@gmail.com>
2025-08-29 23:24:59 -0300

    92. Aplicando Programação Orientada a Objetos básica

---
+++ Funcionario.java
@@ -0,0 +1,28 @@
+public class Funcionario {
+    String nome;
+    String cargo;
+    int idade;
+
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.nome = "Adelmo";
+        funcionario1.cargo = "Desenvolvedor";
+        funcionario1.idade = 43;
+
+        Funcionario funcionario2 = new Funcionario();
+        funcionario2.nome = "Maria";
+        funcionario2.cargo = "Gerente";
+        funcionario2.idade = 50;
+
+        System.out.println("Nome: " + funcionario1.nome);
+        System.out.println("Cargo: " + funcionario1.cargo);
+        System.out.println("Idade: " + funcionario1.idade);
+
+        System.out.println("------------------------------");
+
+        System.out.println("Nome: " + funcionario2.nome);
+        System.out.println("Cargo: " + funcionario2.cargo);
+        System.out.println("Idade: " + funcionario2.idade);
+
+    }
+}
